be7cbd87109e13d5cbb0bca65c6ef0eea7bc4361
nemozrj <nemozrj@outlook.com>
2023-05-22 21:04:22 -0500

    3-6 完成电子书列表列表查询接口

---
+++ src/main/java/com/runjie/wiki/service/EbookService.java
@@ -0,0 +1,21 @@
+package com.runjie.wiki.service;
+
+import com.runjie.wiki.domain.Ebook;
+import com.runjie.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list(){
+        return ebookMapper.selectByExample(null);
+    }
+
+
+
+}
