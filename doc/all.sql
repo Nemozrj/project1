3e207f2a83d6bede74309b611ab3663ce1c602ed
nemozrj <nemozrj@outlook.com>
2023-05-22 19:56:10 -0500

    3-5 集成mybatis generator, 生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -7,3 +7,14 @@ create table test
     password varchar(50) comment '密码'
 )
     comment '测试' engine = innodb charset=utf8mb4;
+
+drop table if exists demo;
+create table demo
+(
+    id   bigint      not null comment 'id'
+        primary key,
+    name varchar(50) null comment '名称'
+)
+    comment '测试' engine = innodb charset=utf8mb4;
+
+insert into demo (id,name) values(1, '测试')
